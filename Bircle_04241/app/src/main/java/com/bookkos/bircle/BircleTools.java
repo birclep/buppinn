411c09577a61f93dcb6279cf0a41ac42fdc294ca
zhangmingkong <zhangzhao290@gmail.com>
2015-11-29 12:24:06 +0900

    add android app
    Bircle 04241 is the android application

---
+++ Bircle_04241/app/src/main/java/com/bookkos/bircle/BircleTools.java
@@ -0,0 +1,8 @@
+package com.bookkos.bircle;
+
+/**
+ * Created by yk.Leonier on 2015/05/27.
+ */
+public class BircleTools {
+    static final String BircleHome="http://130.158.80.42:80";
+}
---
+++ Bircle_04241/app/src/main/java/com/bookkos/bircle/RecognitionActivity.java
@@ -0,0 +1,36 @@
+package com.bookkos.bircle;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.LinearLayout;
+
+import com.bookkos.bircle.camera.CameraManager;
+
+public class RecognitionActivity extends Activity {
+    private CameraManager cameraManager;
+    private ViewfinderView viewfinderView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognition);
+
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        Context context = this;
+        CameraPreview cameraPreview = new CameraPreview(this);
+        this.setContentView(cameraPreview, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+
+    }
+
+}
